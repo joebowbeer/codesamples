acb3064d05c0a829a0fcc95cb4e4da18b6dee3e8
Joe Bowbeer <joe.bowbeer@gmail.com>
2015-09-17 17:04:41 -0700

    Parse Roman numerals and English numerals.

--- src/main/java/joebowbeer/Util.java
+++ src/main/java/joebowbeer/Util.java
@@ -1,5 +1,6 @@
 package joebowbeer;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -7,7 +8,9 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /** Tool class containing coding exercises. */
 public class Util {
@@ -118,5 +121,120 @@ public class Util {
     }
     // Return null if not found
     return null;
+  }
+
+  /* 5 */
+
+  public static int parseRomanNumerals(CharSequence s) {
+    Map<Character, Integer> map = Collections.unmodifiableMap(Stream.of(
+        new SimpleEntry<>('I', 1),
+        new SimpleEntry<>('V', 5),
+        new SimpleEntry<>('X', 10),
+        new SimpleEntry<>('L', 50),
+        new SimpleEntry<>('C', 100),
+        new SimpleEntry<>('D', 500),
+        new SimpleEntry<>('M', 1000))
+        .collect(Collectors.toMap((e) -> e.getKey(), (e) -> e.getValue())));
+    int total = 0;
+    int adjacent = 0;
+    for (int index = s.length(); --index >= 0; ) {
+      int value = map.get(s.charAt(index));
+      if (value < adjacent) {
+        total -= value;
+        adjacent = 0;
+      } else {
+        total += value;
+        adjacent = value;
+      }
+    }
+    return total;
+  }
+
+  /* 6 */
+
+  public static int parseEnglishNumerals(String s) {
+    Map<String, Integer> dict = Collections.unmodifiableMap(Stream.of(
+        new SimpleEntry<>("one", 1),
+        new SimpleEntry<>("two", 2),
+        new SimpleEntry<>("three", 3),
+        new SimpleEntry<>("four", 4),
+        new SimpleEntry<>("five", 5),
+        new SimpleEntry<>("six", 6),
+        new SimpleEntry<>("seven", 7),
+        new SimpleEntry<>("eight", 8),
+        new SimpleEntry<>("nine", 9),
+        new SimpleEntry<>("ten", 10),
+        new SimpleEntry<>("eleven", 11),
+        new SimpleEntry<>("twelve", 12),
+        new SimpleEntry<>("thirteen", 13),
+        new SimpleEntry<>("fourteen", 14),
+        new SimpleEntry<>("fifteen", 15),
+        new SimpleEntry<>("sixteen", 16),
+        new SimpleEntry<>("seventeen", 17),
+        new SimpleEntry<>("eighteen", 18),
+        new SimpleEntry<>("nineteen", 19),
+        new SimpleEntry<>("twenty", 20),
+        new SimpleEntry<>("thirty", 30),
+        new SimpleEntry<>("forty", 40),
+        new SimpleEntry<>("fifty", 50),
+        new SimpleEntry<>("sixty", 60),
+        new SimpleEntry<>("seventy", 70),
+        new SimpleEntry<>("eighty", 80),
+        new SimpleEntry<>("ninety", 90),
+        new SimpleEntry<>("hundred", 100),
+        new SimpleEntry<>("thousand", 1000),
+        new SimpleEntry<>("million", 1000000),
+        new SimpleEntry<>("billion", 1000000000))
+        .collect(Collectors.toMap((e) -> e.getKey(), (e) -> e.getValue())));
+
+    IntStream values = Arrays.stream(s.replaceAll(" and ", " ").split(" ")).mapToInt(dict::get);
+
+    // The following expression correctly evaluates a surprising number of cases.
+    // return values.reduce(0, (acc, e) -> (acc != 0 && acc < e) ? acc * e : acc + e);
+
+    Eval tree = new Eval();
+    values.forEachOrdered(tree::append);
+    return tree.eval();
+  }
+
+  static class Eval {
+
+    static class Node {
+
+      Node left;
+      int value;
+      Node right;
+ 
+      Node(int value) {
+        this.value = value;
+      }
+
+      Node(Node left, int value) {
+        this.left = left;
+        this.value = value;
+      }
+
+      int eval() {
+        return ((left != null) ? left.eval() : 1) * value + ((right != null) ? right.eval() : 0);
+      }
+    }
+
+    Node root;
+
+    void append(int value) {
+      if (root == null || value > root.value) {
+        root = new Node(root, value);
+      } else if (value < root.value) {
+        Node node;
+        for (node = root; node.right != null && node.right.value > value; node = node.right) { }
+        node.right = new Node(node.right, value);
+      } else {
+        throw new IllegalArgumentException();
+      }
+    }
+
+    int eval() {
+      return (root != null) ? root.eval() : 0;
+    }
   }
 }
--- src/test/java/joebowbeer/UtilTest.java
+++ src/test/java/joebowbeer/UtilTest.java
@@ -63,4 +63,46 @@ public class UtilTest {
     assertNull(Util.findFirstUniqueCharacter(""));
     assertNull(Util.findFirstUniqueCharacter("aabbcc"));
   }
+
+  @Test
+  public void testParseRomanNumerals() {
+    assertEquals(1, Util.parseRomanNumerals("I"));
+    assertEquals(2, Util.parseRomanNumerals("II"));
+    assertEquals(3, Util.parseRomanNumerals("III"));
+    assertEquals(4, Util.parseRomanNumerals("IV"));
+    assertEquals(5, Util.parseRomanNumerals("V"));
+    assertEquals(6, Util.parseRomanNumerals("VI"));
+    assertEquals(7, Util.parseRomanNumerals("VII"));
+    assertEquals(8, Util.parseRomanNumerals("VIII"));
+    assertEquals(9, Util.parseRomanNumerals("IX"));
+    assertEquals(10, Util.parseRomanNumerals("X"));
+    assertEquals(11, Util.parseRomanNumerals("XI"));
+    assertEquals(12, Util.parseRomanNumerals("XII"));
+    assertEquals(1492, Util.parseRomanNumerals("MCDXCII"));
+    assertEquals(1958, Util.parseRomanNumerals("MCMLVIII"));
+    assertEquals(2015, Util.parseRomanNumerals("MMXV"));
+  }
+
+  @Test
+  public void testParseEnglishNumerals() {
+    assertEquals(1, Util.parseEnglishNumerals("one"));
+    assertEquals(2, Util.parseEnglishNumerals("two"));
+    assertEquals(3, Util.parseEnglishNumerals("three"));
+    assertEquals(4, Util.parseEnglishNumerals("four"));
+    assertEquals(5, Util.parseEnglishNumerals("five"));
+    assertEquals(6, Util.parseEnglishNumerals("six"));
+    assertEquals(7, Util.parseEnglishNumerals("seven"));
+    assertEquals(8, Util.parseEnglishNumerals("eight"));
+    assertEquals(9, Util.parseEnglishNumerals("nine"));
+    assertEquals(10, Util.parseEnglishNumerals("ten"));
+    assertEquals(11, Util.parseEnglishNumerals("eleven"));
+    assertEquals(1016, Util.parseEnglishNumerals("one thousand and sixteen"));
+    assertEquals(1492, Util.parseEnglishNumerals("fourteen hundred ninety two"));
+    assertEquals(1958, Util.parseEnglishNumerals("one thousand nine hundred fifty eight"));
+    assertEquals(2015, Util.parseEnglishNumerals("two thousand fifteen"));
+    assertEquals(52100, Util.parseEnglishNumerals("fifty two thousand and one hundred"));
+    assertEquals(152000, Util.parseEnglishNumerals("one hundred fifty two thousand"));
+//    assertEquals(1492, Util.parseEnglishNumerals("fourteen ninety two"));
+//    assertEquals(2015, Util.parseEnglishNumerals("twenty fifteen"));
+  }
 }
